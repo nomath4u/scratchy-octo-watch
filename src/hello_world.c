4393ad56e9504f8768796a001c4aaec2e5e43807
Chris Harper <nomath4u@gmail.com>
2014-08-23 11:03:38 -0700

    Successfully subscribed to accelerometer data packets. Now to do something with them

--- src/hello_world.c
+++ src/hello_world.c
@@ -3,6 +3,7 @@
 static Window *window;
 static TextLayer *text_layer;
 
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Double");
   vibes_double_pulse();
@@ -24,6 +25,13 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+static void accel_data_handler(AccelData *data, uint32_t num_samples){
+  // Process 10 events every second
+  // For now just vibrate regardless of data for testing
+  vibes_short_pulse();
+}
+
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -41,6 +49,8 @@ static void window_unload(Window *window) {
 static void init(void) {
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
+  accel_data_service_subscribe(10, accel_data_handler);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
@@ -52,6 +62,7 @@ static void init(void) {
 static void deinit(void) {
   window_destroy(window);
 }
+
 
 int main(void) {
   init();
